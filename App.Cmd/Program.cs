c509ca9d122f96dfbf26fb40521c628529de913d
LouisJagueneau <louis.jagueneau@viacesi.fr>
2024-02-23 14:19:32 +0100

    #110 Mise en place de la mono instance pour la console

--- App.Cmd/Program.cs
+++ App.Cmd/Program.cs
@@ -1,12 +1,21 @@
 ﻿using App.Cmd.Views;
 using App.Core.Services;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace App.Cmd
 {
     internal class Program
     {
+        //Mutex to have app in mono-instance
+        static Mutex mutex = new Mutex(true, "{EasySaveMutex}");
         static void Main(string[] args)
         {
+            if (!mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                return;
+            }
+
+            mutex.ReleaseMutex();
             // Check if the arguments are null
             ArgumentNullException.ThrowIfNull(args);
 
